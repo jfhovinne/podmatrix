eed02bf0615021378c27546a5f3416b4c5f68e64
jfhovinne <git@hovinne.com>
2023-02-05 14:19:09 +0100

    feat: Parallel execution

--- src/exec_command.sh
+++ src/exec_command.sh
@@ -4,29 +4,41 @@ target=${args[--target]}
 eval "images=(${args[--image]})"
 eval "tags=(${args[--tag]})"
 exitcode=0
+i=0
+pids=()
+
+exec() {
+  set +e
+  if [[ -n ${source} ]] && [[ -n ${target} ]]; then
+    >&2 echo "Copying ${source} to ${target}"
+    podman cp $source $CT:$target
+    >&2 echo "Executing command"
+    podman exec $CT bash -c "cd $target && ${command}"
+    ((exitcode+=$?))
+  else
+    >&2 echo "Executing command"
+    podman exec $CT bash -c "${command}"
+    ((exitcode+=$?))
+  fi
+  set -e
+  >&2 echo "Stopping ${image}:${tag}"
+  podman stop $CT > /dev/null
+  >&2 echo "Removing ${image}:${tag}"
+  podman rm $CT > /dev/null
+}
 
 for image in "${images[@]}"; do
   for tag in "${tags[@]}"; do
     >&2 echo "Launching ${image}:${tag}"
     CT=$(podman run -itd ${image}:${tag} /bin/sh)
-    set +e
-    if [[ -n ${source} ]] && [[ -n ${target} ]]; then
-      >&2 echo "Copying ${source} to ${target}"
-      podman cp $source $CT:$target
-      >&2 echo "Executing command"
-      podman exec $CT bash -c "cd $target && ${command}"
-      ((exitcode+=$?))
-    else
-      >&2 echo "Executing command"
-      podman exec $CT bash -c "${command}"
-      ((exitcode+=$?))
-    fi
-    set -e
-    >&2 echo "Stopping ${image}:${tag}"
-    podman stop $CT > /dev/null
-    >&2 echo "Removing ${image}:${tag}"
-    podman rm $CT > /dev/null
+    exec "$CT" &
+    pids[${i}]=$!
+    ((i+=1))
   done
+done
+
+for pid in ${pids[*]}; do
+  wait $pid
 done
 
 exit $exitcode
